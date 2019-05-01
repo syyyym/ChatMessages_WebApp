1339936061bf89f8aee72f4daccfa7f8de20d87b
syyyym <gabi.lupusor@gmail.com>
2019-05-01 19:49:47 +0000

    add express app instance server and routing

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log("Web app");
+'use strict'
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res, next) => {
+    res.send('<h1>Hello Express!</h1>');
+});
+app.get('/dashboard', (req, res, next) => {
+    res.send('<h1>This is the dashboard page</h1>');
+});
+app.listen(3000, () => {
+    console.log("Chat App running on port 3000")
+});
