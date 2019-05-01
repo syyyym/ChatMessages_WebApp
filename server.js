dfbd4ad26a5d728dde843de5ac27fd30de99bbf5
syyyym <gabi.lupusor@gmail.com>
2019-05-01 19:58:42 +0000

    set port from environment var,add it to express config settings table

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict'
 const express = require('express');
 const app = express();
+app.set('port', process.env.PORT || 3000);
 
 app.get('/', (req, res, next) => {
     res.send('<h1>Hello Express!</h1>');
@@ -8,6 +9,6 @@ app.get('/', (req, res, next) => {
 app.get('/dashboard', (req, res, next) => {
     res.send('<h1>This is the dashboard page</h1>');
 });
-app.listen(3000, () => {
-    console.log("Chat App running on port 3000")
+app.listen(app.get('port'), () => {
+    console.log("Chat Application running on port 3000")
 });
