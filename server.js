d0467d79601bb0de21888915e9d6a61ecc586410
syyyym <gabi.lupusor@gmail.com>
2019-05-02 23:15:23 +0000

    serve static assets from /public folder

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const express = require('express');
 const app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'ejs');
+//app.set('views', './views' );
 
+app.use(express.static('public'));
 app.get('/', (req, res, next) => {
     //res.sendFile( __dirname + '/views/login.htm')
     res.render('login', {
