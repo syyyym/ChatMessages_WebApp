40399fa2c7de021f80557858b87c90d2ed02fc2b
syyyym <gabi.lupusor@gmail.com>
2019-05-02 21:57:29 +0000

    render login page dinamically, inject pageTitle with ejs

--- server.js
+++ server.js
@@ -2,9 +2,13 @@
 const express = require('express');
 const app = express();
 app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res, next) => {
-    res.sendFile( __dirname + '/views/login.htm')
+    //res.sendFile( __dirname + '/views/login.htm')
+    res.render('login', {
+        pageTitle: 'My Login Page'
+    });
 });
 
 app.listen(app.get('port'), () => {
