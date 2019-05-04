da30c7ac6b45fa382bc152bf820e9859531dcb2a
syyyym <gabi.lupusor@gmail.com>
2019-05-04 23:42:49 +0000

    bring /app module, mount router instance middleware

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
 'use strict'
 const express = require('express');
 const app = express();
+const chatApp = require('./app');
+
 app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'ejs');
-//app.set('views', './views' );
-
 app.use(express.static('public'));
-app.get('/', (req, res, next) => {
-    //res.sendFile( __dirname + '/views/login.htm')
-    res.render('login', {
-        pageTitle: 'My Login Page'
-    });
-});
+app.use('/', chatApp.router);
 
 app.listen(app.get('port'), () => {
     console.log("Chat Application running on port 3000")
