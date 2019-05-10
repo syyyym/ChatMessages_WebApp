6c4731416dd724811ed39864715cfc152f3f602a
syyyym <gabi.lupusor@gmail.com>
2019-05-10 16:23:24 +0000

    change variables for mongoose connection

--- app/db/index.js
+++ app/db/index.js
@@ -1,8 +1,8 @@
 'use strict';
 const config = require('../config');
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 mongoose.connect(config.dbURI, { useUnifiedTopology: true, useNewUrlParser: true });
-var db = mongoose.connection;
+const db = mongoose.connection;
 
 db.on('error', error => console.error('Database connection error', error));
 db.once('open', function () {
