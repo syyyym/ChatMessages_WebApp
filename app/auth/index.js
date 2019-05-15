5412bda2bd16b34bd2cfb87de0d44c493dfaa27a
syyyym <gabi.lupusor@gmail.com>
2019-05-15 10:51:51 +0000

    add passport verifier fucntion- creating/retrieving user based on fb profileid

--- app/auth/index.js
+++ app/auth/index.js
@@ -1,11 +1,21 @@
 'use strict';
-const passport = require('passport'); 
-const config = require('../config'); 
-const FacebookStrategy = require('passport-facebook').Strategy; 
+const passport = require('passport');
+const config = require('../config');
+const helper = require('../helpers');
+const FacebookStrategy = require('passport-facebook').Strategy;
 
-module.exports = () => { 
-    let authProcessor = (accessToken, refreshToken, profile, done) => { 
-
+module.exports = () => {
+	let authProcessor = (accessToken, refreshToken, profile, done) => {
+		helper.findOne(profile.id)
+			.then(result => {
+				if (result) {
+					done(null, result);
+				} else {
+					helper.createNewUser(profile)
+						.then(createNewUser => done(null, createNewUser))
+						.catch(error => console.log('Error creating a new user in the db', error))
+				}
+			});
 	}
-    passport.use(new FacebookStrategy(config.fb, authProcessor)); 														
+	passport.use(new FacebookStrategy(config.fb, authProcessor));
 }
