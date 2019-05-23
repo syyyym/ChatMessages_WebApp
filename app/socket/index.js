ba5c84e85e6f15175802784203859899979f22c3
syyyym <gabi.lupusor@gmail.com>
2019-05-23 20:21:09 +0000

    client and server connection handshake for namespace /roomlist

--- app/socket/index.js
+++ app/socket/index.js
@@ -1,5 +1,7 @@
 'use strict';
 module.exports = (io, app) => {
     const allRooms = app.locals.chatrooms;
-
+    io.of('/roomslist').on('connection', socket => {
+        console.log('Socket.io connected to client');
+    })
 }
