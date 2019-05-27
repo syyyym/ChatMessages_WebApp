8f05bf52b823254141a3576371d56e2b8988137b
syyyym <gabi.lupusor@gmail.com>
2019-05-27 12:38:48 +0000

    render chatrooms list html using array set from server

--- app/socket/index.js
+++ app/socket/index.js
@@ -1,7 +1,21 @@
 'use strict';
 module.exports = (io, app) => {
-    const allRooms = app.locals.chatrooms;
+    let allrooms = app.locals.chatrooms;
+    allrooms.push({
+        room: 'Good Food',
+        roomID: '0001',
+        users: []
+    });
+    allrooms.push({
+        room: 'Cloud Computing',
+        roomID: '0002',
+        users: []
+    });
+
     io.of('/roomslist').on('connection', socket => {
         console.log('Socket.io connected to client');
+        socket.on('getChatrooms', () => {
+            socket.emit('chatRoomsList', JSON.stringify(allrooms));
+        });
     })
 }
