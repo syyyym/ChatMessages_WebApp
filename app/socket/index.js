577982e9c1b2ddf41d143e0751d2db9dd75907e5
syyyym <gabi.lupusor@gmail.com>
2019-05-29 10:45:22 +0000

    createRoom button - send input text to server, receive updated array

--- app/socket/index.js
+++ app/socket/index.js
@@ -1,7 +1,9 @@
 'use strict';
+const helper = require('../helpers');
+
 module.exports = (io, app) => {
     let allrooms = app.locals.chatrooms;
-    allrooms.push({
+    /*allrooms.push({
         room: 'Good Food',
         roomID: '0001',
         users: []
@@ -10,12 +12,24 @@ module.exports = (io, app) => {
         room: 'Cloud Computing',
         roomID: '0002',
         users: []
-    });
+    }); */
 
     io.of('/roomslist').on('connection', socket => {
         console.log('Socket.io connected to client');
         socket.on('getChatrooms', () => {
             socket.emit('chatRoomsList', JSON.stringify(allrooms));
         });
+
+        socket.on('createNewRoom', newRoomInput => {
+            if (!helper.findRoomByName(allrooms, newRoomInput)) {
+                allrooms.push({
+                    room: newRoomInput,
+                    roomID: helper.randomHex(),
+                    users: []
+                });
+                socket.emit('chatRoomsList', JSON.stringify(allrooms));
+                socket.broadcast.emit('chatRoomsList', JSON.stringify(allrooms));
+            }
+        })
     })
 }
